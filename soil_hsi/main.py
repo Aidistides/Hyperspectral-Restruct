4e6325f7810a7c4915361e923a2daf9f6467ec0e
Aiden Young <208755803+Aidistides@users.noreply.github.com>
2026-04-03 10:36:19 -0400

    Update main.py

--- soil_hsi/main.py
+++ soil_hsi/main.py
@@ -5,3 +5,17 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+from datasets.karlsruhe import KarlsruheDataset
+
+def main():
+    dataset = KarlsruheDataset()
+    dataset.load()
+
+    data = dataset.get_data()
+
+    print(data["spectra"].shape)
+
+if __name__ == "__main__":
+    main()
